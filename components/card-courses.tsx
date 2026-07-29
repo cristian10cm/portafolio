7319c730c76ac68db929c111a6834722617d0692
cristian10cm <cm2005cristian@gmail.com>
2026-07-29 18:29:49 -0500

    Last vs

--- components/card-courses.tsx
+++ components/card-courses.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const  CardCourses=({img,text,year}:{img:string,text:string,year:string}) => {
   return (
     <div className='p-8 h-full justify-between rounded-2xl w-80 flex items-center gap-5 flex-col '>
-       <img src={img} className='w-45 rounded-2xl' alt={text}/>
+       <img src={img} className='w-35 rounded-2xl' alt={text}/>
        <p className='text-pretty text-center font-semibold text-purple-600'>{text}</p> 
        <p className='font-light text-white'>{year}</p>
     </div>
